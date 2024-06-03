b3e9529e97c77ebe47993cd3306748f03875adb4
violatitova <vasya_jewelry@mail.ru>
2024-06-03 11:54:07 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,7 +1,7 @@
-Number_of_homework_done = '12'
-Number_of_hours = '1.5'
+Number_of_homework_done = 12
+Number_of_hours = 1.5
 Name_of_course = 'Python'
-print(1.5/12)
-Time_for_task = '0.125'
+print(Number_of_hours / Number_of_homework_done)
+Time_for_task = 0.125
 print('Курс:', Name_of_course , 'всего задач:' , Number_of_homework_done , ',затрачено часов:' , Number_of_hours , ',среднее время выполнения:' , Time_for_task)
 
